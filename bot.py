348121dddf5466c28618958d74339692f1b771c5
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2024-11-15 18:34:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,16 @@ from pathlib import Path
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("cinemagoer").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("aiohttp").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
 from pyrogram import Client, idle
-from database.ia_filterdb import Media, Media2
+from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import *
 from utils import temp
@@ -57,7 +63,6 @@ async def start():
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
     await Media.ensure_indexes()
-    await Media2.ensure_indexes()
     me = await TechVJBot.get_me()
     temp.BOT = TechVJBot
     temp.ME = me.id
